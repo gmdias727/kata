baa3dd6bd586f4a220063a6daff732336e569d33
Gabriel Dias Mazieri <gabrieldias7200@gmail.com>
2023-06-24 16:08:46 -0300

    New checkpoint! Sat 16:08 Jun 24 2023

--- big-O-Notation/dotnet/big-o-notation/Program.cs
+++ big-O-Notation/dotnet/big-o-notation/Program.cs
@@ -2,10 +2,16 @@
 
 class Program
 {
-    // struct BigONotation
-    // {
-    //     int[] theArray;
-    // }
+
+    struct
+    List<int> the_array = new List<int>();
+    int array_size;
+    int items_in_array;
+
+    public void add_item_to_array(int new_item)
+    {
+        the_array.Add(new_item);
+    }
 
     public static void Main()
     {
