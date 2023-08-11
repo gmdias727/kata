267f788e9357637d1346884fd474349575e9bb9e
Gabriel Mazieri <gabrieldias7200@gmail.com>
2023-08-11 07:04:55 -0300

    New checkpoint! Fri 07:04 Aug 11 2023

---
+++ go_with_tests/mocking/trash/countdown.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+const finalWord = "Go!"
+const countdownStart = 3
+
+type SpySleeper struct {
+	Calls int
+}
+
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+type DefaultSleeper struct{}
+
+type Sleeper interface {
+	Sleep()
+}
+
+const write = "write"
+const sleep = "sleep"
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		sleeper.Sleep()
+	}
+
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+	}
+
+	fmt.Fprint(out, finalWord)
+}
+
+func main() {
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
+}
