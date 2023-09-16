bd18074dac5027e79b716e2c887b331b37d0bd62
Gabriel Mazieri <gabrieldias7200@gmail.com>
2023-09-16 08:51:51 -0300

    New checkpoint! sáb 08:51 set 16 2023

--- go_with_tests/build_an_application/main.go
+++ go_with_tests/build_an_application/main.go
@@ -5,12 +5,6 @@ import (
 	"net/http"
 )
 
-type InMemoryPlayerStore struct{}
-
-func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
-	return 123
-}
-
 func main() {
 	server := &PlayerServer{&InMemoryPlayerStore{}}
 	log.Fatal(http.ListenAndServe(":5000", server))
--- go_with_tests/build_an_application/server.go
+++ go_with_tests/build_an_application/server.go
@@ -9,6 +9,7 @@ import (
 // This interface is used to define a contract for any type that wishes to act as a player score storage.
 type PlayerStore interface {
 	GetPlayerScore(name string) int
+	RecordWin(name string)
 }
 
 // This struct represents the web server that will handle player score requests.
@@ -18,48 +19,28 @@ type PlayerServer struct {
 
 // This method is used to handle incoming HTTP requests.
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-
-	switch r.Method {
-	case http.MethodPost:
-		p.processWin(w)
-	case http.MethodGet:
-		p.showScore(w, r)
-	}
-
-}
-
-func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request) {
-
-	if r.Method == http.MethodPost {
-		w.WriteHeader(http.StatusAccepted)
-		return
-	}
-
 	// Inside this method, it extracts the player's name from the request URL (after removing the "/players/" prefix).
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
 
+	switch r.Method {
+	case http.MethodPost:
+		p.processWin(w, player)
+	case http.MethodGet:
+		p.showScore(w, player)
+	}
+}
+
+func (p *PlayerServer) showScore(w http.ResponseWriter, player string) {
 	score := p.store.GetPlayerScore(player)
 
 	if score == 0 {
 		w.WriteHeader(http.StatusNotFound)
 	}
 
-	fmt.Fprint(w, GetPlayerScore(player))
+	fmt.Fprint(w, score)
 }
 
-func (p *PlayerServer) processWin(w http.ResponseWriter) {
+func (p *PlayerServer) processWin(w http.ResponseWriter, player string) {
+	p.store.RecordWin(player)
 	w.WriteHeader(http.StatusAccepted)
-}
-
-// Given a name return the player score
-func GetPlayerScore(name string) string {
-	if name == "Pepper" {
-		return "20"
-	}
-
-	if name == "Floyd" {
-		return "10"
-	}
-
-	return ""
 }
