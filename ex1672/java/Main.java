a8bce5d758ff523972263c75acec01298c62b729
Gabriel Mazieri <gabrieldias7200@gmail.com>
2024-04-10 14:09:43 -0300

    Make function maximumWealth return an integer

--- ex1672/java/Main.java
+++ ex1672/java/Main.java
@@ -5,13 +5,13 @@ public class Main {
         int[][] array = { {1,2,3}, {3,2,1} };
         maximumWealth(array);
     }
-    public static void maximumWealth(int[][] accounts) {
+    public static int maximumWealth(int[][] accounts) {
         for (int i = 0; i < accounts.length; i++) {
             int sum = 0;
             for (int j = 0; j < accounts[i].length; j++) {
                 sum += accounts[i][j];
             }
-            System.out.printf("Sum of row %d = %d\n", i+1, sum);
+            return sum;
         }
     }
 }
