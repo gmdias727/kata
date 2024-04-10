691c6333bf101509ca97750396330dd3d2fd9b03
Gabriel Mazieri <gabrieldias7200@gmail.com>
2024-04-10 14:09:52 -0300

    aa

--- ex1672/java/Main.java
+++ ex1672/java/Main.java
@@ -6,12 +6,16 @@ public class Main {
         maximumWealth(array);
     }
     public static int maximumWealth(int[][] accounts) {
+        int maxWealth = 0;
         for (int i = 0; i < accounts.length; i++) {
             int sum = 0;
             for (int j = 0; j < accounts[i].length; j++) {
                 sum += accounts[i][j];
             }
-            return sum;
+            if (sum > maxWealth) {
+                maxWealth = sum;
+            }
         }
+        return maxWealth;
     }
 }
